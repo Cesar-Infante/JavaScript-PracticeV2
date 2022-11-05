b956c9da76a1505ca85c13011269f394f757f500
Cesar's PC <cesar261110@gmaill.com>
2022-11-04 18:35:36 -0700

    added todays day

--- client/src/components/DayTime.jsx
+++ client/src/components/DayTime.jsx
@@ -3,13 +3,23 @@ import React from "react";
 /* 1. Write a JavaScript program to display the current day
 and time in the following format.
 
-Sample Output : Today is : Tuesday.
+Sample Output : Today is : Tuesday. ✅
 Current time is : 10 PM : 30 : 38 */
 
 const DayTime = () => {
-    const ex = 'Test';
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const current = new Date();
+    const date = days[current.getDay()];
     return (
-        <div>Hello this is a  {ex} </div>
+        <div>The current day is {date} </div>
     );
 };
 
