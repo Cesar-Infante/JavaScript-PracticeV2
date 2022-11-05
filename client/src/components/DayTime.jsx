b8450b035e194c5a5031a30b11cfc5ae8fee46f6
Cesar's PC <cesar261110@gmaill.com>
2022-11-04 19:28:17 -0700

    Complete time and date element with a bit of styling to test out

--- client/src/components/DayTime.jsx
+++ client/src/components/DayTime.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/DayTime.css";
 
 /* 1. Write a JavaScript program to display the current day
 and time in the following format.
 
 Sample Output : Today is : Tuesday. ✅
-Current time is : 10 PM : 30 : 38 */
+Current time is : 10 PM ✅*/
 
 const DayTime = () => {
     const days = [
@@ -18,8 +19,12 @@ const DayTime = () => {
     ];
     const current = new Date();
     const date = days[current.getDay()];
+    const hour = current.toLocaleString('en-US', { hour: 'numeric', hour12: true});
     return (
-        <div>The current day is {date} </div>
+      <div className="Time">
+        <p className="text">The current day is {date}</p>
+        <p className="text">The current time is {hour}</p>
+      </div>
     );
 };
 
